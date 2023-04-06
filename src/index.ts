271990b776fcbc1194a4526dad59caee4d30bb2c
AndreiValentinStan <andrei_valentin.stan@yahoo.com>
2023-04-06 15:37:57 +0300

    Fixed eslint warning

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import {API} from 'homebridge';
 
 import {PLATFORM_NAME} from './settings';
-import {GreeAirConditionerPlatform} from "./gree/GreeAirConditionerPlatform";
+import {GreeAirConditionerPlatform} from './gree/GreeAirConditionerPlatform';
 
 export = (api: API) => {
     api.registerPlatform(PLATFORM_NAME, GreeAirConditionerPlatform);
